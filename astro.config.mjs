143d7762e8530c5bdbbf507b44c7c04a2aa31a4f
Felipe Maier <Felipe.maier@icloud.com>
2026-03-31 10:10:31 -0300

    Configura GitHub Actions e deploy automático no GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://felipemai3r.github.io',
+  base: '/Projeto_Udesc_Make',
   vite: {
     plugins: [tailwindcss()]
   }
